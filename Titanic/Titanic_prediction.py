3a3d070b7fcd69867b1fd7ad9cacdda598bbc6ef
fantasy <jingxiaofei1@jd.com>
2018-09-13 21:06:40 +0800

    optimization

---
+++ Titanic/Titanic_prediction.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import pandas as pd
+from pandas import Series,DataFrame
+import numpy as np
+
+data_train = pd.read_csv("train.csv")
+#print(data_train)
